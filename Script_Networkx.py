b2dd622e846ef39c304bdc057baaec102000f631
Jalvara <jose.alvarenga@unah.edu.hn>
2023-03-28 18:42:16 -0600

    16_Actualizacion

--- Script_Networkx.py
+++ Script_Networkx.py
@@ -3,6 +3,27 @@ import json
 import matplotlib.pyplot as plt
 import pygraphviz as pgv
 
+def ImprimirGrafo(G,colors):
+    fig, ax=plt.subplots(1,1,figsize=(14,10))
+    pos=nx.drawing.nx_agraph.graphviz_layout(G,prog='neato')
+    nx.draw(G,pos,ax=ax,node_size=200,node_color=colors)
+    nx.draw_networkx_labels(G,pos=pos,ax=ax,font_size=6)
+    #nx.draw_networkx_edges(g,pos=pos,ax=ax,edgelist=LT,width=2)
+    #nx.draw_networkx_edges(g,pos=pos,ax=ax,edgelist=LP,edge_color="blue")
+    #ax.set_title("Conecciones del metro")
+    plt.show()
+#Esta funcion dibujara un subgrafo resaltando sus aristas para la visualizacion rapida del 
+#subgrafo.    
+def ImprimirSubgrafo(G,G1,colors,colors1):
+    fig, ax=plt.subplots(1,1,figsize=(14,10))
+    pos=nx.drawing.nx_agraph.graphviz_layout(G,prog='neato')
+    nx.draw(G,pos,ax=ax,node_size=200,node_color=colors)
+    nx.draw(G1,pos,ax=ax,node_size=200,node_color=colors1)
+    nx.draw_networkx_labels(G,pos=pos,ax=ax,font_size=6)
+    #nx.draw_networkx_edges(G,pos=pos,ax=ax,edgelist=G1.edges(),width=4)
+    nx.draw_networkx_edges(G,pos=pos,ax=ax,edgelist=G1.edges(),edge_color="red")
+    #ax.set_title("Conecciones del metro")
+    plt.show()
 #Cargar la informacion del archivos json
 with open("tokyo-metro.json") as f:
     data=json.load(f)
@@ -37,11 +58,26 @@ LT=[e for e in g.edges() if not g.get_edge_data(*e)['transfer']]
 colors=[data[n[0].upper()]['color'] for n in g.nodes()]
 #print(colors)
 #Parte de la visualizacion del grafo:
-fig, ax=plt.subplots(1,1,figsize=(14,10))
-pos=nx.drawing.nx_agraph.graphviz_layout(g,prog='neato')
-nx.draw(g,pos,ax=ax,node_size=200,node_color=colors)
-nx.draw_networkx_labels(g,pos=pos,ax=ax,font_size=6)
-nx.draw_networkx_edges(g,pos=pos,ax=ax,edgelist=LT,width=2)
-nx.draw_networkx_edges(g,pos=pos,ax=ax,edgelist=LP,edge_color="blue")
-ax.set_title("Conecciones del metro")
-plt.show()
+#ImprimirGrafo(g,colors)
+print("Numero de nodos: ",g.number_of_edges())
+print("Numero de aristas: ",g.number_of_nodes())
+print("Nodos adyacentes de",'C12',list(g.neighbors('C12')))
+#Determinando si g contiene un camino especifico:
+print("Existe un camino entre M19, C12 y M19?: ",nx.is_path(g,['M19','C12','C13']))
+#Determinando el peso de un camino especifico:
+Camino=['C'+str(k) for k in range(1,13)]
+print(Camino)
+print("Peso del camino C1 a C12: ", nx.path_weight(g,Camino,'weight'))
+#Problema del camino mas corto desde una o mas fuentes. 
+g1=nx.shortest_path(g,'C1','M21','weight')
+print('Camino encontrado: ',g1)
+l1=[(g1[k],g1[k+1]) for k in range(len(g1)-1)]
+g1=nx.DiGraph()
+g1.add_edges_from(l1)
+print('Aristas del nuevo grafo: ',g1.edges())
+colors1=[data[n[0].upper()]['color'] for n in g1.nodes()]
+#ImprimirGrafo(g1,colors1)
+#ImprimirSubgrafo(g,g1,colors,colors1)
+#Calculando todos los caminos simples desde un nodo
+g1=nx.single_source_shortest_path(g,'C1')
+print(g1)
